04b3bc2256c8fb90143ce47ddefb02fa794dad6c
Lukas Carvajal <lukascarvajal@gmail.com>
2015-06-23 03:45:30 -0400

    Add a StaticPages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def schools
+  end
+end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get schools" do
+    get :schools
+    assert_response :success
+  end
+
+end
